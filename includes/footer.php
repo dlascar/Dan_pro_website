7f7672940a68a1c61fc8e3dfaf92cf2f619c9cc0
dlascar <34897729+dlascar@users.noreply.github.com>
2018-01-09 17:09:32 -0600

    first_day_footer
    First bit of work on the footer branch. Does not remotely work.

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+	<div class="container">
+		<p class="text-muted">Copyright &copy; 2017 <a href="mailto:lascar@anl.gov?Subject=Website%20question" target="_top">Daniel D. Lascar</a>. Version 2.0. All rights reserved.</p>
+	</div>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,29 @@
+
+    <!-- <meta charset="utf-8"> 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="The contact information of Daniel D. Lascar, Ph.D.">
+    <meta name="author" content="Daniel Lascar">
+    <link rel="icon" href="images/physics-multi-size.ico">
+
+    <title>Contact information of Daniel D. Lascar, Ph.D.</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="dist/css/bootstrap.css" rel="stylesheet">
+	<!--<link href="dist/css/bootstrap.min.css" rel="stylesheet">-->
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <!--<link href="assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">-->
+
+    <!-- Custom styles for this template -->
+    <link href="sticky-footer-navbar.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	
+	<!-- Custom styles for this template -->
+    <!--<link href="carousel.css" rel="stylesheet"> -->
