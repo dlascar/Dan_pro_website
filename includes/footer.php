32a4f02db6a2a4e1b0d63a99247a96be61d433b4
dlascar <34897729+dlascar@users.noreply.github.com>
2018-01-13 16:16:28 -0600

    ver3.1
    Changed footer to version 3.1 reflect the completion of the header change

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,3 @@
 <div class="container">
-	<p class="text-muted">&copy; 2018 <a href="mailto:lascar@anl.gov?Subject=Website%20question" target="_top">Daniel D. Lascar</a>. Version 3.0. All rights reserved.</p>
+	<p class="text-muted">&copy; 2018 <a href="mailto:lascar@anl.gov?Subject=Website%20question" target="_top">Daniel D. Lascar</a>. Version 3.1. All rights reserved.</p>
 </div>
