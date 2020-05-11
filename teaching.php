6d8029ac997bf6062dd6534cf4b1eb2639e13a82
dlascar <34897729+dlascar@users.noreply.github.com>
2020-05-11 02:06:26 -0500

    Better Teaching Vid
    Embedded a better teaching sample from May 6, 2020

--- teaching.php
+++ teaching.php
@@ -75,7 +75,8 @@
 						<h3 class="panel-title">Recent Lecture</h3>
 					</div>
 					<div class="panel-body">
-						<iframe width="100%" src="https://www.youtube-nocookie.com/embed/VSnW576ceYk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						<iframe width="100%" src="https://www.youtube.com/embed/T-jr3D1OWxA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						<!--<iframe width="100%" src="https://www.youtube-nocookie.com/embed/VSnW576ceYk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
 					</div>
 				</div>
 			</div>
